921e4e1c564d65e0d722e99525c601beaa90d41b
Facumko <facumirchenko@gmail.com>
2025-11-06 09:37:50 -0300

    s

--- src/Api/Api.jsx
+++ src/Api/Api.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 // CONFIGURACIÓN
 // ============================================
 
-const API_URL = import.meta.env.VITE_API_URL || "http://192.168.1.64:8080";
+const API_URL = import.meta.env.VITE_API_URL || "https://superadditional-septariate-olevia.ngrok-free.dev";
 const TIMEOUT = 15000;
 const MAX_RETRIES = 2;
 const isDevelopment = import.meta.env.MODE === 'development';
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,27 +2,33 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Home from "../pages/Home";
-import Negocios from "../pages/Negocios";   
+import Negocios from "../pages/Negocios";  
 import Eventos from "../pages/Eventos";
 import Login from "../pages/Login";
-import Profile from "../pages/Profile"; // ← Este import
-import SearchPage from "../pages/SearchPage";
+import Profile from "../pages/Profile";
+import ProfileHeader from "../components/ProfileHeader/ProfileHeader";
+import MyCommerce from "../components/MyCommerce/MyCommerce";
+import Favorites from "../components/Favorites/Favorites";
+
+
 const AppRoutes = () => {
     return(
     <Router>
         <Navbar/>
             <Routes>
-                <Route path="/" element={<Home />} />     
-                <Route path="/negocios" element={<Negocios />} /> 
-                <Route path="/negocios/:id" element={<Negocios />} />
+                <Route path="/" element={<Home />} />    
+                <Route path="/negocios" element={<Negocios />} />
                 <Route path="/Mycommerce" element={<Negocios isOwner={true} />} />
                 <Route path="/profile" element={<Profile />} />
                 <Route path="/eventos" element={<Eventos />} />
-                <Route path="/search" element={<SearchPage />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/profile-header" element={<ProfileHeader isOwner={true} />} />
+                <Route path="/my-commerce" element={<MyCommerce />} />
+                <Route path="/favorites" element={<Favorites />} />
             </Routes>
     </Router>
     );
 };
+
 
 export default AppRoutes;
